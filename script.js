0341c21f3210744d6c07445e07088b55c41a2a4d
Olex <jsonapiqa@gmail.com>
2022-05-16 22:02:33 +0300

    lesson 27

--- script.js
+++ script.js
@@ -1,66 +1,43 @@
-// Что за function такой мы узнаем совсем скоро :) Это нужно для автоматических тестов
-// Пока нас интересуют циклы и условия
-
-function firstTask() {
-    for (let i = 5; i < 11; i++) {
-        console.log(i);
-    }
-}
-
-firstTask()
-
-function secondTask() {
-    for (let i = 20; i >= 10; i--) {
-        if (i === 13) break;
-        console.log(i)
-    }
-}
-
-secondTask()
-
-function thirdTask() {
-    for (let i = 2; i <= 10; i++) {
-        if (i % 2 === 0) {
-            console.log(i);
-        }
-    }
-}
-
-thirdTask()
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-function fourthTask() {
-    let i = 2;
-
-    while (i <= 16) {
-        if (i % 2 === 0) {
-            i++;
-            continue;
-        } else {
-            console.log(i);
-        }
-        i++;
-    }
-}
-
-fourthTask()
-
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    for (let i = 5; i < 11; i++) {
-        arrayOfNumbers[i - 5] = i;
-    }
-
-    console.log(arrayOfNumbers);
-    return arrayOfNumbers;
-}
-
 fifthTask()
+
+function stringName(num) {
+    console.log([num - 1, num, num + 1])
+}
+
+stringName(9)
+
+
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+
+sayHello('Alex');
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+
+returnNeighboringNumbers(5);
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
