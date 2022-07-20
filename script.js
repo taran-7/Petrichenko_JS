5dbe0456526d081feb1295e7e8a2838b3da07e9d
taran-7 <taran76@gmail.com>
2022-07-20 10:37:28 +0300

    up_23

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-let num = 70;
-while (num < 77) {
-  num++;
-  console.log(num);
-}
-
-let num1 = 70;
-do {
-  console.log(num1);
-  num1++;
-} while (num1 < 77);
-
 for (let i = 1; i <= 8; i++) {
   console.log(i);
-}
 
-for (let i = 1; i <= 8; i++) {
-  if (i === 7) {
-    continue;
-    // break
-  }
-  console.log(i);
+  for (let j = 0; j > 12; j++) {}
 }
