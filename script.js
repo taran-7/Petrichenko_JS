e54ac925c8d5272084485a9e021f3d6701efdfbe
taran-7 <taran76@gmail.com>
2022-07-20 10:25:33 +0300

    up

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-/* Задание на урок:
+let num = 70;
+while (num < 77) {
+  num++;
+  console.log(num);
+}
 
-1) Первую часть задания повторить по уроку
+let num1 = 70;
+do {
+  console.log(num1);
+  num1++;
+} while (num1 < 77);
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-"use strict";
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
+for (let i = 1; i <= 8; i++) {
+  console.log(i);
+}
 
 for (let i = 1; i <= 8; i++) {
   if (i === 7) {
-    // continue;
-    break
+    continue;
+    // break
   }
   console.log(i);
 }
