0cadb77d0f2a98e8d3569390a790209dfecd15a6
taran-7 <taran76@gmail.com>
2022-07-26 11:02:41 +0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // СЛАВА ІСУСУ ХРИСТУ!
 "use strict";
 
-if (7 == 7) {
+if (7 == 8) {
   console.log("Ok!");
+} else {
+  console.log("Error");
+}
+
+const num = 50;
+
+if (num < 49) {
+  console.log("error");
+} else if (num > 100) {
+  console.log("много");
+} else {
+  console.log("Как раз то, что нужно");
 }
