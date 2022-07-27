73351247ad0bcc145376229edf30aaa7c9ba4294
taran-7 <taran76@gmail.com>
2022-07-27 10:24:03 +0300

    up 20 lesson

--- script.js
+++ script.js
@@ -7,12 +7,30 @@ if (7 == 8) {
   console.log("Error");
 }
 
+// const num = 50;
+
+// if (num < 49) {
+//   console.log("error");
+// } else if (num > 100) {
+//   console.log("много");
+// } else {
+//   console.log("Как раз то, что нужно");
+// }
+
+// (num === 50) ? console.log("Ok") : console.log("error");
+
 const num = 50;
 
-if (num < 49) {
-  console.log("error");
-} else if (num > 100) {
-  console.log("много");
-} else {
-  console.log("Как раз то, что нужно");
+switch (num) {
+  case: 49
+  console.log("вгадав");
+  break;
+  case: 51
+  console.log("вгаfdgdfдав");
+  break;
+  case: 99
+  console.log("вгадdfgfdgfdав");
+  break;
+  case: 88
+  console.log("вгадghjhgjав");
 }
