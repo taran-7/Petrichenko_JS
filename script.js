4b73d10b75863f53bdf1f81e181f566695da8a27
taran-7 <taran76@gmail.com>
2022-08-03 00:08:30 +0300

    up

--- script.js
+++ script.js
@@ -68,3 +68,22 @@ if (hamburger === 3 && cola === 2 || fries === 3 && fries) {
   console.log('Йдемо в інший заклад!')
 }
 console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
+
+
+
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
