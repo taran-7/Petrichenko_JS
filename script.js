ea3f4b2844e65c7c34bfaf91f6d2ba7573cc6fc4
taran-7 <taran76@gmail.com>
2022-08-10 22:42:53 +0300

    up21

--- script.js
+++ script.js
@@ -87,3 +87,19 @@ console.log(NaN || null || !3 || undefined || 5);
 console.log(NaN || null && !3 && undefined || 5);
 
 console.log(5 === 5 && 3 > 1 || 5);
+
+console.log( NaN || 2 || undefined );
+
+console.log( NaN && 2 && undefined );
+
+console.log( 1 && 2 && 3 );
+
+console.log( !1 && 2 || !3 );
+
+console.log( 25 || null && !3 );
+
+console.log( NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
