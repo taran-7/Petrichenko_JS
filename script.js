037c7d807f2d0f1fa59d4550b10e09054a7f47c8
taran-7 <taran76@gmail.com>
2022-08-16 10:07:06 +0300

    add arrey methods

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 /*jshint esversion: 6 */
 "use strict";
 
+const people = [
+    { name: 'Vasyl', age: 33, hith: 177 },
+    { name: 'Mykola', age: 54, hith: 157 },
+    { name: 'Petro', age: 22, hith: 189 },
+]
+
+for (let i = 0; i < people.length; ++i) {
+    console.log(people[i])
+}
+
+for (let person of people) {
+    console.log(person)
+}
+
