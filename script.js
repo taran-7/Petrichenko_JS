4d5c85be7a141ef6e13a86ad77b5e82d8ab168d9
taran-7 <taran76@gmail.com>
2022-08-23 00:06:58 +0300

    asterix

--- script.js
+++ script.js
@@ -4,12 +4,25 @@
 
 // Циклы та мітки. Урок 23 Івана Петриченко
 
-for (let i = 0; i <3; i++){
-    console.log(i, 'i')
-    for(let j=0; j<4;j++){
-        console.log(j, 'j')
-            }
+// for (let i = 0; i <3; i++){
+//     console.log(i, 'i')
+//     for(let j=0; j<4;j++){
+//         console.log(j, 'j')
+//             }
+// }
+
+let result = ''
+const length = 7
+
+for (let i = 1; i<length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*'
+    }
+    result += '\n';
 }
+console.log(result)
+
 
 
 
