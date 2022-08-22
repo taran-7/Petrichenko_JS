1167a82d30ccdd548e35e66c9eaf61a5b5058300
taran-7 <taran76@gmail.com>
2022-08-22 11:12:27 +0300

    up

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 /*jshint esversion: 6 */
 "use strict";
 
-// Циклы и метки
+// Циклы та мітки. Урок 23 Івана Петриченко
 
+for (let i = 0; i <3; i++){
+    console.log(i, 'i')
+    for(let j=0; j<4;j++){
+        console.log(j, 'j')
+            }
+}
 
 
 
